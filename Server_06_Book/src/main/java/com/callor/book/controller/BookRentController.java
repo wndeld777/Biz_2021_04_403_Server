09cbaf36344a597cfa1693cfc6cb11af21a2d7e1
wndeld777 <wndeld777@gmail.com>
2021-05-06 16:39:43 +0900

    home

--- Server_06_Book/src/main/java/com/callor/book/controller/BookRentController.java
+++ Server_06_Book/src/main/java/com/callor/book/controller/BookRentController.java
@@ -2,6 +2,8 @@ package com.callor.book.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -12,12 +14,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.callor.book.model.BookDTO;
 import com.callor.book.model.BookRentDTO;
 import com.callor.book.model.BookRentVO;
 import com.callor.book.model.BuyerDTO;
 import com.callor.book.service.BookRentService;
+import com.callor.book.service.BookService;
 import com.callor.book.service.BuyerService;
 import com.callor.book.service.impl.BookRentServiceImplV1;
+import com.callor.book.service.impl.BookServiceImplV1;
 import com.callor.book.service.impl.BuyerServiceImplV1;
 
 /*
@@ -32,10 +37,12 @@ public class BookRentController extends HttpServlet {
 	private static final long serialVersionUID = 921652892464670154L;
 	protected BookRentService brService;
 	protected BuyerService buService;
+	protected BookService bkService;
 
 	public BookRentController() {
 		brService = new BookRentServiceImplV1(); // new... 추가하기
 		buService = new BuyerServiceImplV1();
+		bkService = new BookServiceImplV1();
 	}
 
 	@Override
@@ -66,16 +73,11 @@ public class BookRentController extends HttpServlet {
 
 				// view에서 보여줄 데이터 생성
 				/*
-				 * ServletContext
-				 * Tomcat을 기반으로 작성된 Web App Service에서
-				 * 요청(Req) 응답(Res)를 총괄하는 정보가 담긴 객체
-				 * Web App Service를 구현하기 위하여
-				 * Req, Res 를 처리하는 여러가지 기능을 구현해야 하는데
-				 * 그러한 기능을 미리 구현해 놓았기 때문에
-				 * ServletContext를 getter 하는 것만으로 충분하다
+				 * ServletContext Tomcat을 기반으로 작성된 Web App Service에서 요청(Req) 응답(Res)를 총괄하는 정보가
+				 * 담긴 객체 Web App Service를 구현하기 위하여 Req, Res 를 처리하는 여러가지 기능을 구현해야 하는데 그러한 기능을 미리
+				 * 구현해 놓았기 때문에 ServletContext를 getter 하는 것만으로 충분하다
 				 * 
-				 * DB 등으로부터 조회된 데이터를 Web 에게 응답하고자 할때
-				 * 쉬운 방법으로 전달할 수 있도록 하는 기능이 미리 구현되어 있다
+				 * DB 등으로부터 조회된 데이터를 Web 에게 응답하고자 할때 쉬운 방법으로 전달할 수 있도록 하는 기능이 미리 구현되어 있다
 				 */
 				ServletContext app = this.getServletContext();
 				// bService가 return한 brDTO를
@@ -92,7 +94,7 @@ public class BookRentController extends HttpServlet {
 				// Rendering을 하라
 				// webapp/WEB_INF/views/book.jsp 파일을 읽어서
 				// Java코드로 변환하고, 실행할 준비를하라
-				RequestDispatcher disp = app.getRequestDispatcher("/WEB-INF/views/book.jsp");
+				RequestDispatcher disp = app.getRequestDispatcher("/WEB-INF/views/order_info.jsp");
 				// Rendering된 view 데이터를
 				// Web browser를 response 하라
 				disp.forward(req, resp);
@@ -109,58 +111,118 @@ public class BookRentController extends HttpServlet {
 		} else if (subPath.equals("/buyer")) {
 			// 회원코드로 찾기
 			brService.findByBuyerCode("buyercode");
-			
+
 			// rent/order 로 요청하면 주문서 작성 처음화면 보여주기
 			// 회원이름을 입력하는 화면을 보여주기
 		} else if (subPath.equals("/order")) {
-			
+
 			ServletContext app = req.getServletContext();
 			RequestDispatcher disp = req.getRequestDispatcher("/WEB-INF/views/order.jsp");
 			disp.forward(req, resp);
-		
-		}else if(subPath.equals("/order/page1")) {
-			
+
+		} else if (subPath.equals("/order/page1")) {
+
 			String bu_name = req.getParameter("bu_name");
-			if(bu_name == null || bu_name.equals("")) {
+			if (bu_name == null || bu_name.equals("")) {
 				out.println("회원 이름을 반드시 입력해야 합니다");
 				out.close();
-			}else {
+			} else {
 				List<BuyerDTO> buList = buService.findByName(bu_name);
-				
+
 				// Service에서 전달된 데이터가 잘 왔나? (확인코드)
 				System.out.println("=".repeat(50));
-				for(BuyerDTO d : buList) {
+				for (BuyerDTO d : buList) {
 					System.out.println(d.toString());
 				}
 				System.out.println("=".repeat(50));
-				
-				
-				ServletContext app = req.getServletContext();
-				app.setAttribute("BUYERS",buList);
-				
+
+				// ServletContext를 생성하여 속성(변수) 세팅하기
+				// ServletContext app = req.getServletContext();
+				// app.setAttribute("BUYERS",buList);
+
+				// req 객체에 바로 세팅하기
+				req.setAttribute("BUYERS", buList);
+
+				// page1.jsp 파일을 열고 BUYERS 변수와 함께
+				// Rendering을 하여 HTML 코드를 생성하라
 				RequestDispatcher disp = req.getRequestDispatcher("/WEB-INF/views/page1.jsp");
 				disp.forward(req, resp);
 			}
-			
-			
-		}else if(subPath.equals("/order/page2")) {
+
+		} else if (subPath.equals("/order/page2")) {
 			String bu_code = req.getParameter("bu_code");
-			
+			// bu_code 값에 해당하는 회원정보 추출
 			BuyerDTO buyerDTO = buService.findById(bu_code);
-			
-			ServletContext app = req.getServletContext();
-			app.setAttribute("BUYER", buyerDTO);
-			
+			if (buyerDTO != null) {
+				// bu_code 값에 해당하는 회원정보가 있으면 Console에 출력
+				System.out.println(buyerDTO.toString());
+			}
+			// ServletContext app = req.getServletContext();
+			// app.setAttribute("BUYER", buyerDTO);
+			req.setAttribute("BUYER", buyerDTO);
+
+			// BUYER에 담긴 회원정보를 page2.jsp에 Rendering 하여 보여라
 			RequestDispatcher disp = req.getRequestDispatcher("/WEB-INF/views/page2.jsp");
-			
+
 			disp.forward(req, resp);
-			
+		} else if (subPath.equals("/order/book")) {
+			String bu_code = req.getParameter("bu_code");
+			String bk_title = req.getParameter("bk_title");
+			if (bk_title == null || bk_title.equals("")) {
+				out.println("도서명을 입력하세요");
+				out.close();
+			} else {
+				// 회원정보를 한번더 조회
+				BuyerDTO buDTO = buService.findById(bu_code);
+				req.setAttribute("BUYER", buDTO);
+
+				List<BookDTO> bookList = bkService.findByTitle(bk_title);
+				req.setAttribute("BOOKS", bookList);
+				// method chaining 방식으로 연속 호출하기
+				req.getRequestDispatcher("/WEB-INF/views/book.jsp").forward(req, resp);
+
+			}
+		} else if (subPath.equals("/order/insert")) {
+
+			String bk_isbn = req.getParameter("bk_isbn");
+			String bu_code = req.getParameter("bu_code");
+
+			// 대여일자 값을 생성하기 위하여
+			// 날짜클래스와 날짜포멧클래스를 사용하여
+			// 대여일자 문자열 만들기
+			// 현재 컴퓨터 시스템 날짜 가져오기
+			Date date = new Date(System.currentTimeMillis());
+
+			// 날짜 데이터를 문자열로 변환하기 위한 설정
+			SimpleDateFormat sd = new SimpleDateFormat("yyyy-MM-dd");
+
+			// 날짜 데이터를 설정한 포멧대로 문자열로 변환
+			String sDate = sd.format(date);
+			System.out.println("대여일자 : " + sDate);
+
+			// INSERT 를 수행하기 위해
+			// VO를 만들고 web에서 전달받은
+			// 도서 ISBN과 회원 CODE를 Setting
+			BookRentVO brVO = new BookRentVO();
+			brVO.setBr_sdate(sDate);
+			brVO.setBr_isbn(bk_isbn);
+			brVO.setBr_bcode(bu_code);
+			brVO.setBr_price(1000);
+			int result = brService.insert(brVO);
+			if (result > 0) {
+				out.println("대여정보 추가 성공!!!");
+			} else {
+				out.println("대여정보 추가 실패!!!");
+			}
+			out.close();
 		} else if (subPath.equals("/return")) {
 			// 반납하기
 			BookRentVO bookRentVO = new BookRentVO();
 			brService.update(bookRentVO);
 		} else {
 			// 더이상 그만하기
+			out.println("NOT FOUND");
+			out.close();
 		}
 
 	}
