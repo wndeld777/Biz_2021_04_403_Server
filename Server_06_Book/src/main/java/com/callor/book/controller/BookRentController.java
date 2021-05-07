cd2e1c53692116adb3e2ba41e3aab2a01fd1e483
wndeld777 <wndeld777@gmail.com>
2021-05-07 16:38:12 +0900

    setAttribute

--- Server_06_Book/src/main/java/com/callor/book/controller/BookRentController.java
+++ Server_06_Book/src/main/java/com/callor/book/controller/BookRentController.java
@@ -210,9 +210,11 @@ public class BookRentController extends HttpServlet {
 			brVO.setBr_price(1000);
 			int result = brService.insert(brVO);
 			if (result > 0) {
-				out.println("대여정보 추가 성공!!!");
+				// out.println("대여정보 추가 성공!!!");
+				resp.sendRedirect("/book/");
 			} else {
 				out.println("대여정보 추가 실패!!!");
+				resp.sendRedirect("/book/order");
 			}
 			out.close();
 		} else if (subPath.equals("/return")) {
